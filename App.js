1f8e69b3c4459f39bfba02d0ea248543759ac0ec
oKenzz <ken98ph@gmail.com>
2024-04-12 17:03:35 +0200

    Co-authored-by: Ismail Sacic <ismail.sacic@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SearchBar, Button } from '@rneui/base';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
 
 export default function App() {
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+
+  const handleSearch = (search) => {
+    console.log(search); // Debug: log the search variable to ensure it has the expected value
+      fetch(`https://chalmers_grouproom.sacic.dev/api/v1/search/${encodeURIComponent(search)}`,{
+          headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          }
+        }
+      )
+      .then((response) => response.json())
+      .then((json) => {
+        setSearchResult(JSON.stringify(json, null, 2)); // Convert and save the response in state
+      })
+      .catch((error) => {
+        console.error(error);
+        setSearchResult( error.toString() );
+      }
+      );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.searchContainer}>
+        <SearchBar
+          placeholder="Type here"
+          onChangeText={text => setSearch(text)}
+          value={search}
+        />
+        <Button
+          title="Search"
+          onPress={() => handleSearch(search)}
+        />
+      </View>
+      <ScrollView style={styles.resultContainer}>
+        <Text style={styles.resultText}>{searchResult}</Text>
+      </ScrollView>
     </View>
   );
 }
@@ -14,7 +52,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  searchContainer: {
+    paddingTop: 50,
+    paddingBottom: 10,
+    backgroundColor: '#fff',
+  },
+  resultContainer: {
+    flex: 1, // Important to make sure the ScrollView takes up the rest of the space
+    paddingHorizontal: 20,
+  },
+  resultText: {
+    fontFamily: 'monospace', // Helps in displaying JSON text (if supported)
   },
 });
