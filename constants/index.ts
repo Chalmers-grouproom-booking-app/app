d7af9a2563d5f3a2e9afda37e3b4bb567f10a3ad
IsmailR16 <ismaelalhoran585@gmail.com>
2024-04-23 21:33:55 +0200

    Lindholmen Done

--- constants/index.ts
+++ constants/index.ts
@@ -1,20 +1,20 @@
 export const InitRegion = {
     latitude: 57.689131,
     longitude: 11.974257,
-    latitudeDelta: 0.003,
-    longitudeDelta: 0.003,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
 };
 
 export const Johanneberg = {
     latitude: 57.689131,
     longitude: 11.974257,
-    latitudeDelta: 0.003,
-    longitudeDelta: 0.003,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
 }
 
 export const Lindholmen = {
     latitude: 57.7064748,
     longitude: 11.9368681,
-    latitudeDelta: 0.003,
-    longitudeDelta: 0.003,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
 }
