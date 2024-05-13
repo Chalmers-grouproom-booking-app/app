775ad25437c94d6306fbda8b9793e4a3a1bd44c2
oKenzz <ken98ph@gmail.com>
2024-05-13 16:04:45 +0200

    FilterSearch done

--- components/SearchComponent/useRoomSearch.ts
+++ components/SearchComponent/useRoomSearch.ts
@@ -1,15 +1,11 @@
 import { useState, useCallback } from 'react';
-import { FilterData, RoomInfo, BuildingGroup, Interval} from '../../constants/types'; // Importing types from your constants
+import { FilterData, RoomInfo, BuildingGroup, Interval } from '../../constants/types'; // Importing types from your constants
 
 function useRoomSearch() {
     const [searchResult, setSearchResult] = useState<RoomInfo[] | null>(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string>('');
-    interface Params {
-      [key: string]: string;
-    }
-  
-    const params: Params = {};
+    
 
     const createQueryString = (searchQuery: string, filterData: FilterData): string => {
      
@@ -18,33 +14,28 @@ function useRoomSearch() {
             equipment: formatEquipment,
             room_size: formatRoomSize,
             first_come_first_served: formatFirstComeFirstServed
-        }        
+        }
+        
+        interface Params {
+            [key: string]: string;
+          }
+        
+        const params: Params = {};
 
         // Always include 'input', empty if searchQuery is blank
         params.input = searchQuery.trim() !== '' ? encodeURIComponent(searchQuery) : '';
 
         for(const key in filterData){
-            if (filterData[key] !== null){
+            if (filterData[key] !== null && !(Array.isArray(filterData[key]) && filterData[key].length === 0)){
                 params[key] = formatters[key](filterData[key]);
             }
         } 
 
-        // Iterate over filterData keys
-        // Object.keys(filterData).forEach(key => {
-        //   const value = filterData[key];
-        //   if (value !== undefined && value !== null) {
-        //     if (Array.isArray(value) && value.length > 0) {
-        //       params[key] = value.join(',');
-        //     } else if (typeof value === 'string' && value !== '') {
-        //       params[key] = encodeURIComponent(value);
-        //     }
-        //   }
-        // });
       
         // Use URLSearchParams to build the query string
         return new URLSearchParams(params).toString();
       };
-
+    
     const formatBuilding = (building: BuildingGroup): string => {
         let buildingQuery: string = ''
         buildingQuery += building.name 
@@ -74,7 +65,6 @@ function useRoomSearch() {
         try {
             const queryString: string = createQueryString(searchQuery, filterQuery);
             console.log(queryString)
-            console.log(formatBuilding(filterQuery.building)) 
             if (filterQuery.room_size !== null){
                 console.log(formatRoomSize(filterQuery.room_size))
             }
