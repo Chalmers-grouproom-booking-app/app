5d7f54d2f0b1c153370fbfd26e9e9a42c9bc68c3
Melker Gustafsson <smelker02@gmail.com>
2024-05-21 12:03:27 +0200

    reverted quickbook thing

--- components/QuickBookComponent/ResultPanel.tsx
+++ components/QuickBookComponent/ResultPanel.tsx
@@ -40,14 +40,16 @@ const FilterPanel = ({ filter, goBack }: { filter: PanelFilter, goBack: () => vo
                 url.search = params.toString();
                 const response = await fetch(url.toString());
                 const data = await response.json() as RoomData;
-                const filteredRooms: RoomInfoV2[] = [];
+                const tmp_rooms: RoomInfoV2[] = [];
                 for (const key in data) {
                     for (const room of data[key]) {
-                        if (!filteredRooms.find(r => r.room_name === room.room_name) && !room.first_come_first_served) {
-                            filteredRooms.push(room);
+                        // remove duplicates and first come first served rooms
+                        if (!tmp_rooms.some(r => r.room_name === room.room_name)) {
+                            tmp_rooms.push(room);
                         }
                     }
                 }
+                const filteredRooms = tmp_rooms.filter(room => !room.first_come_first_served) as RoomInfoV2[];
                 // magic code
                 // @ts-ignore
                 if(filteredRooms[0] === "N"){
